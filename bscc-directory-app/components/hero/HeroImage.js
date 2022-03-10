bb8eee7b4f64591e367572568deccbac0f06379b
dawlishwarren <dawlishwarren@gmail.com>
2022-03-10 14:50:12 +0000

    hero image on home page

---
+++ bscc-directory-app/components/hero/HeroImage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Image from 'next/image';
+import highStreet from '../../public/img/highstreet.jpeg';
+import styles from '../../styles/Hero.module.css';
+import { ParallaxBanner } from 'react-scroll-parallax';
+
+const HeroImage = () => {
+	return (
+		<ParallaxBanner
+			layers={[
+				{
+					children: (
+						<Image
+							src={highStreet}
+							layout='fill'
+							priority='true'
+							placeholder='blur'
+						/>
+					),
+					speed: -30,
+				},
+				{
+					children: (
+						<div className={styles.header_container}>
+							<h1 className={styles.header}>
+								Budleigh Salterton Chamber of Commerce
+							</h1>
+						</div>
+					),
+					speed: 10,
+				},
+			]}
+			className={styles.container}></ParallaxBanner>
+	);
+};
+export default HeroImage;
